c2d30cd96af78cf8b84a70775a3d3a8cccff7b61
Rushikesh Here <rushikeshhere@gmail.com>
2024-07-25 11:06:24 +0530

    Includes method in ES7

---
+++ Exponential_Operator/02_Array_Include_Method.js
@@ -0,0 +1,6 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+
+// includes() method is only for Array data structure
+
+console.log(myArray.includes(4)); // return true
+console.log(myArray.includes(10)); // return false
